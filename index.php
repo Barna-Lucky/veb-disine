0f94bc9208f883de4858bdc511a1bde779a91594
katee20 <78478961+katee20@users.noreply.github.com>
2021-02-03 13:55:42 +0100

    k

--- index.php
+++ index.php
@@ -1,28 +1,28 @@
 <!DOCTYPE html>
 <html>
-<head>
-  <link rel = "stylesheet"
-   type = "text/css"
-   href = "style.css" />
+  <head>
+    <link rel = "stylesheet"
+     type = "text/css"
+     href = "style.css" />
 
-   <link rel="preconnect" href="https://fonts.gstatic.com">
-<link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+     <link rel="preconnect" href="https://fonts.gstatic.com">
+     <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
 
 
-</head>
-<body>
-  <div id="header">
+  </head>
+  <body>
+    <div id="header">
 
-  <p class="h1"><b>Welcome</b> to this part of the interweb!</p>
+    <p class="h1"><b>Welcome</b> to this part of the interweb!</p>
 
-  </div>
+    </div>
 
-  <div id="main">
-    <p class="title"> </p>
-    <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in eros turpis. Aenean mattis, arcu non dictum fermentum, turpis lectus consectetur mauris, lobortis venenatis felis nisi vel nunc. Maecenas mollis tortor a eros cursus facilisis. Sed convallis elit in feugiat tincidunt. Aliquam erat volutpat. Aenean sit amet egestas nunc, vel efficitur ante. Suspendisse potenti. Ut dolor mauris, lobortis vitae scelerisque finibus, cursus vitae nisi. Nunc efficitur, sem non gravida iaculis, libero ex laoreet ante, eu maximus enim augue sed odio. Maecenas euismod nisl vitae volutpat elementum. Aliquam lacinia augue quis tincidunt varius. Nulla facilisi. Nulla at diam vestibulum lorem auctor commodo sed quis quam.
+    <div id="main">
+      <p class="title"> </p>
+      <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in eros turpis. Aenean mattis, arcu non dictum fermentum, turpis lectus consectetur mauris, lobortis venenatis felis nisi vel nunc. Maecenas mollis tortor a eros cursus facilisis. Sed convallis elit in feugiat tincidunt. Aliquam erat volutpat. Aenean sit amet egestas nunc, vel efficitur ante. Suspendisse potenti. Ut dolor mauris, lobortis vitae scelerisque finibus, cursus vitae nisi. Nunc efficitur, sem non gravida iaculis, libero ex laoreet ante, eu maximus enim augue sed odio. Maecenas euismod nisl vitae volutpat elementum. Aliquam lacinia augue quis tincidunt varius. Nulla facilisi. Nulla at diam vestibulum lorem auctor commodo sed quis quam.
 
-Nullam molestie in enim at suscipit. Nulla eget arcu mattis, congue ipsum accumsan, viverra mauris. Sed at volutpat tellus. Sed laoreet leo leo, vel vestibulum est scelerisque ac. Sed a est tempor, tempus arcu sit amet, consequat tortor. Sed lacinia, libero eget sagittis pretium, erat nunc viverra leo, blandit gravida justo nisi non mi. Praesent ultricies aliquet est, in feugiat sem placerat in. Donec aliquet, lorem et suscipit lobortis, nisl leo molestie libero, sit amet faucibus lectus nisi ut elit. Quisque id vestibulum risus. Praesent a vulputate dui. </p>
-  </div>
+  Nullam molestie in enim at suscipit. Nulla eget arcu mattis, congue ipsum accumsan, viverra mauris. Sed at volutpat tellus. Sed laoreet leo leo, vel vestibulum est scelerisque ac. Sed a est tempor, tempus arcu sit amet, consequat tortor. Sed lacinia, libero eget sagittis pretium, erat nunc viverra leo, blandit gravida justo nisi non mi. Praesent ultricies aliquet est, in feugiat sem placerat in. Donec aliquet, lorem et suscipit lobortis, nisl leo molestie libero, sit amet faucibus lectus nisi ut elit. Quisque id vestibulum risus. Praesent a vulputate dui. </p>
+    </div>
 
-</body>
+  </body>
 </html>
