9d4f2a4ea421a0ddfdb29a687b17fffb9a8d14ae
katee20 <78478961+katee20@users.noreply.github.com>
2021-02-03 13:10:29 +0100

    k

--- index.php
+++ index.php
@@ -1,10 +1,17 @@
 <!DOCTYPE html>
 <html>
-<head> 
+<head>
+  <link rel = "stylesheet"
+   type = "text/css"
+   href = "style.css" />
+
+   <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
 
 
 </head>
 <body>
+  <p><h1><b>Welcome</b> to this part of the interweb!</h1></p>
 
 
 </body>
