6bc60c9fe5b04e873789759aed7d37448c8d2ce6
katee20 <78478961+katee20@users.noreply.github.com>
2021-02-03 13:45:39 +0100

    k

--- index.php
+++ index.php
@@ -12,12 +12,16 @@
 </head>
 <body>
   <div id="header">
-    
-  <p><h1><b>Welcome</b> to this part of the interweb!</h1></p>
+
+  <p class="h1"><b>Welcome</b> to this part of the interweb!</p>
 
   </div>
 
   <div id="main">
+    <p class="title"> </p>
+    <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in eros turpis. Aenean mattis, arcu non dictum fermentum, turpis lectus consectetur mauris, lobortis venenatis felis nisi vel nunc. Maecenas mollis tortor a eros cursus facilisis. Sed convallis elit in feugiat tincidunt. Aliquam erat volutpat. Aenean sit amet egestas nunc, vel efficitur ante. Suspendisse potenti. Ut dolor mauris, lobortis vitae scelerisque finibus, cursus vitae nisi. Nunc efficitur, sem non gravida iaculis, libero ex laoreet ante, eu maximus enim augue sed odio. Maecenas euismod nisl vitae volutpat elementum. Aliquam lacinia augue quis tincidunt varius. Nulla facilisi. Nulla at diam vestibulum lorem auctor commodo sed quis quam.
+
+Nullam molestie in enim at suscipit. Nulla eget arcu mattis, congue ipsum accumsan, viverra mauris. Sed at volutpat tellus. Sed laoreet leo leo, vel vestibulum est scelerisque ac. Sed a est tempor, tempus arcu sit amet, consequat tortor. Sed lacinia, libero eget sagittis pretium, erat nunc viverra leo, blandit gravida justo nisi non mi. Praesent ultricies aliquet est, in feugiat sem placerat in. Donec aliquet, lorem et suscipit lobortis, nisl leo molestie libero, sit amet faucibus lectus nisi ut elit. Quisque id vestibulum risus. Praesent a vulputate dui. </p>
   </div>
 
 </body>
